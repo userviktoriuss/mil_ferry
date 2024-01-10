98659694574c92fdc474b163aae9d0773a9b2bcf
Viktor Filimonov <tf349@mail.ru>
2024-01-11 00:15:13 +0300

    fixed window size

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -1,15 +1,32 @@
 #include "mainwindow.h"
 #include "./ui_mainwindow.h"
-#include "QRegularExpressionValidator"
 
 MainWindow::MainWindow(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+    // Set the desired size and screen-mode type.
+    QScreen *screen = QGuiApplication::primaryScreen ();
+    QRect geom = screen->availableGeometry() ;
+    int screenWidth = geom.width();
+    int screenHeight = geom.height();
+    this->resize(screenWidth, screenHeight);
+    this->setWindowState(Qt::WindowMaximized);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
 }
+
+void MainWindow::on_clearCarPushButton_clicked()
+{
+    ui->callLineEdit->clear();
+    ui->widthLineEdit->clear();
+    ui->lengthLineEdit->clear();
+    ui->xCoordLineEdit->clear();
+    ui->yCoordLineEdit->clear();
+}
+
--- mainwindow.h
+++ mainwindow.h
@@ -2,6 +2,8 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include<QScreen>
+#include <qDebug>
 
 QT_BEGIN_NAMESPACE
 namespace Ui {
@@ -16,6 +18,9 @@ class MainWindow : public QMainWindow
 public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
+
+private slots:
+    void on_clearCarPushButton_clicked();
 
 private:
     Ui::MainWindow *ui;
